adb92e9b0ee21c732f7ed23ffa1cfc6601f47989
amine001 <aminefathi@tutanota.com>
2023-12-19 13:30:56 +0100

    update 3

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,9 @@
+
+
+// Initialize Firebase
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +12,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "blog-cddc2.firebaseapp.com",
-  projectId: "blog-cddc2",
-  storageBucket: "blog-cddc2.appspot.com",
-  messagingSenderId: "413696194974",
-  appId: "1:413696194974:web:bc0a551cd5558bdf0dbe8f"
+  authDomain: "paretohub.firebaseapp.com",
+  projectId: "paretohub",
+  storageBucket: "paretohub.appspot.com",
+  messagingSenderId: "280196376940",
+  appId: "1:280196376940:web:330b783731c2f7d75eeb49"
 };
 
 // Initialize Firebase
